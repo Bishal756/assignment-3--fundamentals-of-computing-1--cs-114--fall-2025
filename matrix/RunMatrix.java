83418a6d16be7d6d88ef607efc61d0034874c7d8
Bishal756 <bk6507074@gmail.com>
2025-11-18 17:33:59 -0500

    RunMatrix.java

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,6 +1,31 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter matrix size: ");
+        int size = keyboard.nextInt();
+        System.out.println();
+
+        Matrix table = new Matrix(size);
+
+        System.out.println("Initial matrix:");
+        table.printMatrix();
+        System.out.println();
+
+        System.out.println("Populating matrix...");
+        table.populateMatrix();
+        System.out.println("Matrix with values:");
+        table.printMatrix();
+        System.out.println();
+
+        System.out.println("Flipping matrix...");
+        table.flipMatrix();
+        System.out.println("Matrix after flip:");
+        table.printMatrix();
+
+        keyboard.close();
+    }
 }
